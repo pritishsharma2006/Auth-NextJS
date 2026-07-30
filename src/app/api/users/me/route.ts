f4820dd48685ae46b9ea9d0d81e03e6521ddd3ba
Pritish Kumar Sharma <sharmapritishkumar@gmail.com>
2026-07-30 20:03:04 +0530

    added middleware/proxy and me route

---
+++ src/app/api/users/me/route.ts
@@ -0,0 +1,22 @@
+import { getDataFromToken } from "@/helper/getDataFromToken";
+
+import { NextRequest,NextResponse } from "next/server";
+import User from "@/model/userModel";
+import { connect } from "@/dbConfig/dbConfig";
+
+connect();
+
+export async function GET(request:NextRequest){
+    try {
+        const userId = await getDataFromToken(request);
+        const user = await User.findOne({_id: userId}).select("-password");
+
+        return NextResponse.json({
+            message: "user found",
+            data: user,
+        })
+
+    } catch (error:any) {
+        return NextResponse.json({error: error.message},{status: 400});
+    }
+}
